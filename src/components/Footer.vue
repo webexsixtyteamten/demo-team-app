978db530b53021e207a97cc96ba6325260321bbd
KojimaShumpei-web <kojishun1021@outlook.jp>
2022-11-16 17:46:50 +0900

    footerを修正しました

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,9 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">○○○, ○○○ and ○○○</span>.
+      <span class="names"
+        >Koudai Takebayashi, Tada Haruki and Kojima Shumpei</span
+      >.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
